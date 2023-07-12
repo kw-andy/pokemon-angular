0c88d6fc77fb30d31478848319e3f2ca7e7c6790
unknown <andy.kwok.work@gmail.com>
2023-07-12 16:03:42 +0200

    adding the 404, the detail pokemon module, the return

--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-pokemon',
@@ -11,5 +12,9 @@ export class ListPokemonComponent {
   
   pokemons_list: Pokemon[] = POKEMONS;
 
+  constructor(private router: Router) {}
 
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/pokemon', pokemon.id]);
+  }
 }
